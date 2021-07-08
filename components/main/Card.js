58b03bdd55ed17311c64a43ccb1583650f3fe1c5
Intiser <irparash@gmail.com>
2021-07-08 12:32:17 -0500

    added empty card and empty event components, added function in feed to fetch posts real-time

---
+++ components/main/Card.js
@@ -0,0 +1,11 @@
+// this is the card component for the posts in the feed
+
+import react from 'react';
+
+const Card = (props) => {
+    return (
+        <div>
+            This is a card
+        </div>
+    )
+}
--- components/main/Feed.js
+++ components/main/Feed.js
@@ -5,13 +5,38 @@
  * Displays main feed
  */
 
-import React from "react";
+import React, {useState} from "react";
 import { View, Text } from "react-native";
+import firebase from 'firebase';
 
-export const Feed = () => {
+const db = firebase.firestore();
+
+// function to filter the posts
+// there will be card components within the view. The card components will be clickable
+// clicking it will redirect the user to the Event page with the event descriptions passed
+// down as props
+const Feed = () => {
+    let [posts, setPosts] = useState([]);
+
+    db.collection('posts').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+            let temp = posts;
+            if (change.type == 'added') {
+                temp.push(change.doc);
+                setPosts(temp);
+            } else if (change.type == 'removed') {
+                setPosts(temp.filter(post => post.id != change.doc.id));
+            }
+        })
+    })
+
     return (
         <View>
-            <Text>Feed</Text>
+            // loop through all posts and inject each one into a card component
+            <Card>
+
+            </Card>
         </View>
     );
 }
