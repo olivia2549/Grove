4f8438bdca5ae1643307a0434e3977c4b85ec1ca
davisglen <davisglen00@yahoo.com>
2021-07-12 20:31:32 -0500

    title of the eventdetail page is set and the scrolldown page transition as well

--- components/main/EventDetails.js
+++ components/main/EventDetails.js
@@ -6,7 +6,7 @@
  */
 
 import React, {useEffect, useState} from "react";
-import { View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions, ScrollView, Animated, } from "react-native";
+import { View, Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions, ScrollView, Animated, Button, } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 
@@ -15,11 +15,13 @@ const windowWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'column',
+        // flexDirection: 'column',
+        flex: 1,
+        justifyContent: "center",
     },
     topBar: {
         backgroundColor: '#5DB075',
-        height: windowHeight * 0.1,
+        height: "20%",
         width: "100%",
         position: "absolute",
         top: 0,
@@ -35,26 +37,63 @@ const styles = StyleSheet.create({
     eventName: {
         color: '#ffffff',
         fontWeight: "600",
+        top: '20%',
+        padding: 10
         // flex: 1,
     },
     scrollable: {
-        flex: 1/2,
-    }
+        top: "14%",
+        // flex: 1/2,
+    },
 })
 
 // function to provide details about each event/card that is present in the feed page
 export const EventDetails = ({navigation, route}) => {
-
     // get the parameters
     const { eventName, eventDay, eventTime, peopleGoing, tags } = route.params;
 
+    // title font size 
+    const [currentFont, setCurrentFont] = useState(windowWidth * 0.12);
+
+    // gestureName is for knowing which gesture direction user executed
+    const [gestureName, setGestureName] = useState("none");
+
+    const onSwipeDown = (gestureState) =>{
+        navigation.goBack();
+    }
+
+    const config = {
+        velocityThreshold: 0.3,
+        directionalOffsetThreshold: 80
+    };
+
     return (
         <SafeAreaView style={styles.container}>
-                <View style={styles.topBar}>
-                    <Text style={styles.eventName}>{eventName}</Text>
-                </View>
+
+            <GestureRecognizer
+                onSwipeDown={(state) => onSwipeDown(state)}
+                config={config}
+                style={styles.topBar}
+                > 
+                    <Text 
+                    numberOfLines={ 1 }
+                    adjustsFontSizeToFit
+                    style={ [styles.eventName, {fontSize: currentFont}]}
+                    onTextLayout={ (e) => {
+                        const { lines } = e.nativeEvent;
+                        if (lines.length > 1) {
+                            setCurrentFont(currentFont - 1);
+                        }
+                    } }
+                    >
+                        {eventName}</Text>
+                </GestureRecognizer>
+    
+
+                
+
+                {/* <View style={{height: 30, width: 100, backgroundColor: "black"}}></View> */}
                 <ScrollView style={styles.scrollable}>
-                    <View >
                         <Text>{eventDay}</Text>
                         <Text>{eventTime}</Text>
                         <Text>{peopleGoing}</Text>
@@ -62,8 +101,11 @@ export const EventDetails = ({navigation, route}) => {
                         {
                             tags.map((tag) => <Text>{tag}</Text>)
                         }
-                    </View>
                 </ScrollView>
+
+                
+
+                
         </SafeAreaView>
     );
 }
