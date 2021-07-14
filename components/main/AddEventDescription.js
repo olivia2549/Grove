fe75d722956df2d8e6c200156b9dc60acd36a4c8
Intiser <irparash@gmail.com>
2021-07-14 11:36:58 -0500

    tags are pressable toggles in event description

--- components/main/AddEventDescription.js
+++ components/main/AddEventDescription.js
@@ -1,28 +1,62 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
 import { addEventDescription } from "../../redux/actions";
 
-const AddEventDescription = () => {
+const AddEventDescription = ( { route } ) => {
     const navigation = useNavigation();
-    const dispatch = useDispatch();
-    const eventName = useSelector((state) => state.event.eventName);
+    const eventName = route.params.eventName;
     const [eventDescription, setEventDescription] = useState("");
-
-    useEffect(() => {
-        console.log(eventName);
-        dispatch(addEventDescription(eventDescription));
-    })
+    const selectedTags = [];
+    const allTags = ["Sports", "Social", "Orgs", "Free food", "Invite-only", "All-welcome", "Party"];
 
     const onChange = (ev) => {
         setEventDescription(ev.target.value);
     }
 
+    const Tag = ( props ) => {
+        const grey = "#f6f7fa";
+        const green = "#5db075";
+        let [color, setColor] = useState(grey);
+
+        const onPress = () => {
+            color == grey ? setColor(green) : setColor(grey);
+            selectedTags.push(props.title);
+        }
+
+        return (
+        <TouchableOpacity
+        onPress={onPress}
+        style={{
+            backgroundColor: color,
+            borderRadius: 5,
+            padding: 5,
+            margin: 5,
+            width: 70,
+        }}
+        activeOpacity={0.6}
+        >
+            <Text>{props.title}</Text>
+        </TouchableOpacity>
+        )
+    } 
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Tags and Description</Text>
-
+            <View >
+            {
+            allTags.map((tag, i) => {
+                return (
+                    <Tag
+                    key={i}
+                    title={tag}                
+                   />
+                )
+            })}
+            </View>
+            
             <TextInput
                 id="eventDescription"
                 name="eventDescription"
