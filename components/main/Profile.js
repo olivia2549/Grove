d0597021edde85941ba46a03a6d5c7006ae97f8b
mrSeoDavid <seodavid221@gmail.com>
2021-07-16 18:41:37 -0500

    profile page stylesheet

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -6,153 +6,272 @@
  */
 
 import React, { useState, useEffect } from "react";
-import {StyleSheet, View, Text, Image, FlatList, Button, TouchableOpacity, Platform, Dimensions} from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  FlatList,
+  Button,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+} from "react-native";
 
 import { useSelector, useDispatch } from "react-redux";
 
 import firebase from "firebase";
-import {USER_POSTS_STATE_CHANGE, USER_STATE_CHANGE} from "../../redux/constants";
-import {clearData} from "../../redux/actions";
-require ('firebase/firestore');
+import {
+  USER_POSTS_STATE_CHANGE,
+  USER_STATE_CHANGE,
+} from "../../redux/constants";
+import { clearData } from "../../redux/actions";
+require("firebase/firestore");
 
-
-const windowHeight = Dimensions.get('window').height;
-const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-    containerInfo: {
-        margin: 20,
-    },
-    containerGallery: {
-        flex: 1
-    },
-    containerImage: {
-        flex: 1/3
-    },
-    image: {
-        flex: 1,
-        aspectRatio: 1
-    },
-    screenContainer: {
-        flex: 1,
-        justifyContent: "center",
-        backgroundColor: "white"
-        // padding: 16
-    },
-})
+  containerInfo: {
+    margin: 20,
+  },
+  containerGallery: {
+    flex: 1,
+  },
+  containerImage: {
+    flex: 1 / 3,
+  },
+  image: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  screenContainer: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+
+  // view
+  infoView: {
+    flex: Platform.OS === "ios" ? 5 : 6,
+    backgroundColor: "white",
+  },
+
+  // user's name
+  userNameContainer: {
+    flex: 2,
+    backgroundColor: "#5DB075",
+    justifyContent: "center",
+  },
+  userNameText: {
+    textAlign: "center",
+    fontWeight: "500",
+    fontSize: windowWidth * 0.084,
+    color: "white",
+    marginBottom: Platform.OS === "ios" ? 0 : 30,
+  },
+
+  // profile pic
+  profileBackground: {
+    width: 110,
+    height: 110,
+    borderRadius: 200,
+    justifyContent: "center",
+    marginLeft: 15,
+    marginTop: -55,
+    backgroundColor: "white",
+  },
+  profilePic: {
+    width: 100,
+    height: 100,
+    borderRadius: 400 / 2,
+    marginLeft: 5,
+    marginBottom: 0,
+  },
+
+  // user info
+  userEmail: {
+    marginLeft: 20,
+    fontSize: windowWidth * 0.045,
+    fontWeight: "400",
+  },
+
+  // add friend
+  addFriend: {
+    flex: 1 / 7,
+    marginHorizontal: windowWidth * 0.028,
+    marginTop: 8,
+    height: "7%",
+    backgroundColor: "#5DB075",
+    borderRadius: 10,
+    justifyContent: "center",
+  },
+  addFriendText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: windowWidth * 0.04,
+  },
+
+  // locked
+  lockContainer: {
+    marginTop: 25,
+    flexDirection: "row",
+  },
+  lockIcon: {
+    justifyContent: "center",
+    marginLeft: 20,
+    width: windowWidth * 0.058,
+    height: windowHeight * 0.037,
+  },
+  lockIconText: {
+    color: "#666666",
+    fontSize: windowWidth * 0.042,
+    marginLeft: 10,
+    marginTop: 2,
+  },
+
+  //sign out
+  signOut: {
+    flex: 1 / 4,
+    marginHorizontal: 12,
+    marginLeft: 18,
+    marginBottom: 6,
+    backgroundColor: "#5DB075",
+    borderRadius: 20,
+    justifyContent: "center",
+  },
+  signOutText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: windowWidth * 0.031,
+  },
+});
 
 export const Profile = (props) => {
-    const [userPosts, setUserPosts] = useState([]);
-    const [user, setUser] = useState(null);
-    const currentUser = useSelector((state) => state.currentUser);
-    const currentUserPosts = useSelector(state => state.currentUser.posts);
-    const dispatch = useDispatch();
+  const [userPosts, setUserPosts] = useState([]);
+  const [user, setUser] = useState(null);
+  const currentUser = useSelector((state) => state.currentUser);
+  const currentUserPosts = useSelector((state) => state.currentUser.posts);
+  const dispatch = useDispatch();
 
-    const signOut = () => {
-        firebase.auth().signOut();
-        dispatch(clearData());
-    };
+  const signOut = () => {
+    firebase.auth().signOut();
+    dispatch(clearData());
+  };
 
-    // Load user, and if different than current user, fetch from database
-    useEffect(() => {
-        // If the uid to display is the current user, our job is easy
-        if (props.route.params.uid === firebase.auth().currentUser.uid) {
-            setUser(currentUser);
-            setUserPosts(currentUserPosts);
-        }
-        // Otherwise, we need to grab a different user and their posts from firebase
-        else {
-            // This is essentially 'fetchUser' from actions/index.js but doesn't change state of application
-            firebase.firestore()
-                .collection("users")
-                .doc(props.route.params.uid)    // This time, grab the uid from what was passed in as a props param
-                .get()
-                .then((snapshot) => {
-                    // if the user exists, change the user state
-                    if (snapshot.exists) {
-                        // Set user to display onscreen
-                        setUser(snapshot.data());
-                    }
-                    else {
-                        console.log("User does not exist.")
-                    }
-                })
-                .catch((error) => {console.log(error)})
-
-            // This is essentially 'fetchUserPosts' from actions/index.js but doesn't change state of application
-            firebase.firestore()
-                .collection("posts")
-                .doc(props.route.params.uid)    // This time, grab the uid from what was passed in as a props param
-                .collection("userPosts")    // fetch everything in the collection
-                .orderBy("creation", "asc") // ascending order by creation date
-                .get()
-                .then((snapshot) => {
-                    // Iterate through everything in the snapshot and build a posts array
-                    let postsArr = snapshot.docs.map(doc => {
-                        const data = doc.data();
-                        const id = doc.id;
-                        return { id, ...data }  // the object to place in the posts array
-                    });
-                    setUserPosts(postsArr);
-                })
-                .catch((error) => {console.log(error)})
-        }
-    }, [props.route.params.uid])    // Only calls useEffect when uid changes (makes app faster)
-
-    if (user === null) {
-        return <View/>
+  // Load user, and if different than current user, fetch from database
+  useEffect(() => {
+    // If the uid to display is the current user, our job is easy
+    if (props.route.params.uid === firebase.auth().currentUser.uid) {
+      setUser(currentUser);
+      setUserPosts(currentUserPosts);
     }
+    // Otherwise, we need to grab a different user and their posts from firebase
+    else {
+      // This is essentially 'fetchUser' from actions/index.js but doesn't change state of application
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(props.route.params.uid) // This time, grab the uid from what was passed in as a props param
+        .get()
+        .then((snapshot) => {
+          // if the user exists, change the user state
+          if (snapshot.exists) {
+            // Set user to display onscreen
+            setUser(snapshot.data());
+          } else {
+            console.log("User does not exist.");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
-    return (
-        <View style={styles.screenContainer}>
-            <View style={{flex: 1, backgroundColor: "#5DB075", justifyContent: "center"}}>
-                <Text style={{textAlign: "center", fontWeight: "500", fontSize: 33, color: "white", marginBottom: Platform.OS === 'ios' ? 0 : 18}}>{user.name}</Text>
-                
-            </View>
+      // This is essentially 'fetchUserPosts' from actions/index.js but doesn't change state of application
+      firebase
+        .firestore()
+        .collection("posts")
+        .doc(props.route.params.uid) // This time, grab the uid from what was passed in as a props param
+        .collection("userPosts") // fetch everything in the collection
+        .orderBy("creation", "asc") // ascending order by creation date
+        .get()
+        .then((snapshot) => {
+          // Iterate through everything in the snapshot and build a posts array
+          let postsArr = snapshot.docs.map((doc) => {
+            const data = doc.data();
+            const id = doc.id;
+            return { id, ...data }; // the object to place in the posts array
+          });
+          setUserPosts(postsArr);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [props.route.params.uid]); // Only calls useEffect when uid changes (makes app faster)
 
-            <View style={{width: windowWidth * 0.25, height: windowHeight * 0.13, borderRadius: 200, justifyContent: "center", marginLeft: 15, marginTop: -45, backgroundColor: "white"}}>
-                <Image 
-                source={require('../../assets/profile.jpeg')}  
-                style={{width: windowWidth * 0.22, height: windowHeight * 0.12 , borderRadius: 400/2, marginLeft: 5}} 
-                /> 
-            </View>
-            
-           
+  if (user === null) {
+    return <View />;
+  }
 
-            <View style={{flex: Platform.OS === 'ios' ? 3 : 4, backgroundColor: "white"}}>
-                
-                <View style={styles.containerInfo}>
-                    
-                    <Text>{user.email}</Text>
-                </View>
-                <View style={styles.containerGallery}>
-                    <FlatList
-                        numColumns={3}
-                        horizontal={false}
-                        data={userPosts}
-                        renderItem={({item}) => (
-                            <View style={styles.containerImage}>
-                                <Image style={styles.image} source={{uri: item.downloadURL}}/>
-                            </View>
-                        )}
-                    />
-                </View> 
-            </View>
+  return (
+    <View style={styles.screenContainer}>
+      <View style={styles.userNameContainer}>
+        <Text style={styles.userNameText}>{user.name}</Text>
+      </View>
 
-           
-            {/* <Button style={{}} title="Sign Out" onPress={signOut}/> */}
+      <View style={styles.profileBackground}>
+        <Image
+          source={require("../../assets/profileicon.jpg")}
+          style={styles.profilePic}
+        />
+      </View>
 
-            
-            
-            {/* <View style={{padding: 15}}> */}
-                <TouchableOpacity onPress={signOut} style={{ width: "90%", marginLeft: 18, marginBottom: 6, height: "4%", backgroundColor: "#5DB075", borderRadius: 20}}>
-                    <Text style={{textAlign:"center", marginTop: 5, color: "white"}}>Sign Out</Text>
-                </TouchableOpacity>
-            {/* </View> */}
-
-            
+      <View style={styles.infoView}>
+        <View style={styles.containerInfo}>
+          <Text style={styles.userEmail}>{user.email}</Text>
         </View>
-    );
-}
+        <TouchableOpacity
+          onPress={() => console.log("Tryna add some good friends")}
+          style={styles.addFriend}
+        >
+          <Text style={styles.addFriendText}>Add Friend</Text>
+        </TouchableOpacity>
+        <View style={styles.lockContainer}>
+          <Image
+            source={require("../../assets/lock_outline.png")}
+            style={styles.lockIcon}
+          />
+          <Text style={styles.lockIconText}>
+            Follow this account to see their events
+          </Text>
+        </View>
+        <View style={styles.containerGallery}>
+          <FlatList
+            numColumns={3}
+            horizontal={false}
+            data={userPosts}
+            renderItem={({ item }) => (
+              <View style={styles.containerImage}>
+                <Image
+                  style={styles.image}
+                  source={{ uri: item.downloadURL }}
+                />
+              </View>
+            )}
+          />
+        </View>
+      </View>
+
+      {/* <Button style={{}} title="Sign Out" onPress={signOut}/> */}
+
+      {/* <View style={{padding: 15}}> */}
+      <TouchableOpacity onPress={signOut} style={styles.signOut}>
+        <Text style={styles.signOutText}>Sign Out</Text>
+      </TouchableOpacity>
+      {/* </View> */}
+    </View>
+  );
+};
 
 export default Profile;
