6d845db02e1eb41f84cb74a9a5e10965c0ff0533
olivia2549 <ologanmail@gmail.com>
2021-07-17 10:20:43 -0500

    resolved conflict with i'm going button

--- components/main/EventDetails.js
+++ components/main/EventDetails.js
@@ -17,6 +17,150 @@ import firebase from "firebase";
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
+// function to provide details about each event/card that is present in the feed page
+export const EventDetails = ({navigation, route}) => {
+    // get the parameters
+    const event = route.params.event.item;
+    const start = parseDate(event.startDateTime.toDate());
+    const end = parseDate(event.endDateTime.toDate());
+    const [goingBtnText, setGoingBtnText] = useState("i'm going");
+    const [goingBtnSelected, setGoingBtnSelected] = useState(false);
+
+    // title font size
+    const [currentFont, setCurrentFont] = useState(50);
+
+    // gestureName is for knowing which gesture direction user executed
+    const [gestureName, setGestureName] = useState("none");
+
+    const onSwipeDown = (gestureState) => {
+        navigation.goBack();
+    }
+
+    const onGoing = () => {
+        setGoingBtnSelected(!goingBtnSelected);
+        goingBtnSelected ? setGoingBtnText("i'm not going") : setGoingBtnText("i'm going");
+        if (goingBtnSelected) {
+            console.log("adding event to users...");
+            firebase.firestore().collection("users")
+                .doc(firebase.auth().currentUser.uid)
+                .update({
+                    eventsAttending: firebase.firestore.FieldValue.arrayUnion(event.id)
+                })
+            // TODO: make this work right
+            firebase.firestore().collection("events")
+                .doc(event.id)
+                .update({
+                    attendee: firebase.firestore.FieldValue.arrayUnion(event)
+                })
+        } else {
+            console.log("removing event from users...")
+            firebase.firestore().collection("users")
+                .doc(firebase.auth().currentUser.uid)
+                .update({
+                    eventsAttending: firebase.firestore.FieldValue.arrayRemove(event)
+                })
+            firebase.firestore().collection("events")
+                .doc(event.id)
+                .update({
+                    attendee: firebase.firestore.FieldValue.arrayRemove(event)
+                })
+        }
+    };
+
+    const config = {
+        velocityThreshold: 0.3,
+        directionalOffsetThreshold: 80
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            {/* <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={{flex: 1}}
+                > */}
+
+            <GestureRecognizer
+                onSwipeDown={(state) => onSwipeDown(state)}
+                config={config}
+                style={styles.topBar}
+                >
+                    <Text
+                    adjustsFontSizeToFit
+                    style={ [styles.eventName, {fontSize: currentFont}]}
+                    onTextLayout={ (e) => {
+                        const { lines } = e.nativeEvent;
+                        if (lines.length > 1) {
+                            setCurrentFont(currentFont - 1);
+                        }
+                    }}
+                    >{event.name}</Text>
+            </GestureRecognizer>
+
+            <ScrollView style={{flex: Platform.OS === 'ios' ? 0 : 7, top: 10}}>
+                <View style={{flexDirection:"row"}}>
+                   {
+                        event.tags.map((tag) =>
+                            <View style={styles.tagBox}>
+                                <Text style={styles.tagText}>{tag}</Text>
+                            </View>
+                        )
+                    }
+                </View>
+                <View style={{padding: windowWidth * 0.05}}>
+                    <Text style={{fontSize: windowWidth * 0.07}}>{event.description}</Text>
+                </View>
+
+                <View style={{justifyContent: "center", padding: windowWidth * 0.05}}>
+                    <View style={{flexDirection: "row"}}>
+                        <Text style={styles.whereWhen}>Where</Text>
+                        <View style={{flex: 1, marginLeft: windowWidth * 0.02, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
+                            {/* this is hard coded, would need to be changed once we fetch info from the data */}
+                            <Text style={{marginLeft: windowWidth * 0.03,  color:"black", fontSize: windowWidth * 0.05}}>{event.location}</Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection: "row", marginTop: 6, marginLeft: 1}}>
+                        <Text style={{fontSize: windowWidth * 0.06, fontWeight: "bold", marginTop: 3}}>Starts</Text>
+
+                        <View style={{flex: 1, marginLeft: 15, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
+                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{start.day}</Text>
+                        </View>
+                        <View style={{flex: 1, marginLeft: 15, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
+                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{start.ampmTime}</Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection: "row", marginTop: 6, marginLeft: 1}}>
+                        <Text  style={{fontSize: windowWidth * 0.06, fontWeight: "bold", marginTop: 2}}>Ends</Text>
+
+                        <View style={{flex: 1, marginLeft: 25, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
+                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{end.day}</Text>
+                        </View>
+                        <View style={{flex: 1, marginLeft: 15, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
+                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{end.ampmTime}</Text>
+                        </View>
+                    </View>
+                </View>
+
+
+                <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{justifyContent: "center", padding: windowWidth * .05, flex: 1}}>
+                    <Text style={{fontSize: windowWidth * 0.07, fontWeight: "bold", marginBottom: windowHeight * 0.01}}t>{event.attendees.length} people going</Text>
+
+                </KeyboardAvoidingView>
+            </ScrollView>
+
+            <View style={{ flexDirection: "row"}}>
+                <TouchableOpacity onPress={() => console.log("share")} style={styles.fancyButtonContainer}>
+                    <Text style={styles.fancyButtonText}>Share</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onGoing}  style={styles.fancyButtonContainer}>
+                    <Text style={styles.fancyButtonText}>{goingBtnText}</Text>
+                </TouchableOpacity>
+            </View>
+
+
+        </SafeAreaView>
+    );
+}
+
 const styles = StyleSheet.create({
     container: {
         // flexDirection: 'column',
@@ -90,152 +234,5 @@ const styles = StyleSheet.create({
         // marginBottom: windowHeight * 0.015,
     },
 })
-
-// function to provide details about each event/card that is present in the feed page
-export const EventDetails = ({navigation, route}) => {
-    // get the parameters
-    // const { name, description, starttime, endtime, location, attendee, tags } = route.params.post.item;
-    const post = route.params.post.item;
-    const start = parseDate(post.starttime.toDate());
-    const end = parseDate(post.endtime.toDate());
-    const [goingBtnText, setGoingBtnText] = useState("i'm going");
-    const [goingBtnSelected, setGoingBtnSelected] = useState(false);
-
-    // title font size
-    const [currentFont, setCurrentFont] = useState(50);
-
-    // gestureName is for knowing which gesture direction user executed
-    const [gestureName, setGestureName] = useState("none");
-
-    const onSwipeDown = (gestureState) => {
-        navigation.goBack();
-    }
-
-    const onGoing = () => {
-        setGoingBtnSelected(!goingBtnSelected);
-        goingBtnSelected ? setGoingBtnText("i'm not going") : setGoingBtnText("i'm going");
-        if (goingBtnSelected) {
-            console.log("adding post to users...");
-            firebase.firestore().collection("users")
-                .doc(firebase.auth().currentUser.uid)
-                .update({
-                    eventsAttending: firebase.firestore.FieldValue.arrayUnion(post.id)
-                })
-            // TODO: make this work right
-            firebase.firestore().collection("posts")
-                .doc(post.id)
-                .update({
-                    attendee: firebase.firestore.FieldValue.arrayUnion(post)
-                })
-        } else {
-            console.log("removing post from users...")
-            firebase.firestore().collection("users")
-                .doc(firebase.auth().currentUser.uid)
-                .update({
-                    eventsAttending: firebase.firestore.FieldValue.arrayRemove(post)
-                })
-            firebase.firestore().collection("posts")
-                .doc(post.id)
-                .update({
-                    attendee: firebase.firestore.FieldValue.arrayRemove(post)
-                })
-        }
-    };
-
-    const config = {
-        velocityThreshold: 0.3,
-        directionalOffsetThreshold: 80
-    };
-
-    return (
-        <SafeAreaView style={styles.container}>
-            {/* <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : "height"}
-                style={{flex: 1}}
-                > */}
-
-            <GestureRecognizer
-                onSwipeDown={(state) => onSwipeDown(state)}
-                config={config}
-                style={styles.topBar}
-                >
-                    <Text
-                    adjustsFontSizeToFit
-                    style={ [styles.eventName, {fontSize: currentFont}]}
-                    onTextLayout={ (e) => {
-                        const { lines } = e.nativeEvent;
-                        if (lines.length > 1) {
-                            setCurrentFont(currentFont - 1);
-                        }
-                    }}
-                    >{post.name}</Text>
-            </GestureRecognizer>
-
-            <ScrollView style={{flex: Platform.OS === 'ios' ? 0 : 7, top: 10}}>
-                <View style={{flexDirection:"row"}}>
-                   {
-                        post.tags.map((tag) =>
-                            <View style={styles.tagBox}>
-                                <Text style={styles.tagText}>{tag}</Text>
-                            </View>
-                        )
-                    }
-                </View>
-                <View style={{padding: windowWidth * 0.05}}>
-                    <Text style={{fontSize: windowWidth * 0.07}}>{post.description}</Text>
-                </View>
-
-                <View style={{justifyContent: "center", padding: windowWidth * 0.05}}>
-                    <View style={{flexDirection: "row"}}>
-                        <Text style={styles.whereWhen}>Where</Text>
-                        <View style={{flex: 1, marginLeft: windowWidth * 0.02, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
-                            {/* this is hard coded, would need to be changed once we fetch info from the data */}
-                            <Text style={{marginLeft: windowWidth * 0.03,  color:"black", fontSize: windowWidth * 0.05}}>{post.location}</Text>
-                        </View>
-                    </View>
-                    <View style={{flexDirection: "row", marginTop: 6, marginLeft: 1}}>
-                        <Text style={{fontSize: windowWidth * 0.06, fontWeight: "bold", marginTop: 3}}>Starts</Text>
-
-                        <View style={{flex: 1, marginLeft: 15, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
-                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{start.day}</Text>
-                        </View>
-                        <View style={{flex: 1, marginLeft: 15, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
-                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{start.ampmTime}</Text>
-                        </View>
-                    </View>
-                    <View style={{flexDirection: "row", marginTop: 6, marginLeft: 1}}>
-                        <Text  style={{fontSize: windowWidth * 0.06, fontWeight: "bold", marginTop: 2}}>Ends</Text>
-
-                        <View style={{flex: 1, marginLeft: 25, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
-                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{end.day}</Text>
-                        </View>
-                        <View style={{flex: 1, marginLeft: 15, justifyContent: "center", height: windowHeight * 0.055, backgroundColor: "lightgrey", borderRadius: 10, }}>
-                            <Text style={{marginLeft: windowWidth * 0.03, color:"black", fontSize: windowWidth * 0.05}}>{end.ampmTime}</Text>
-                        </View>
-                    </View>
-                </View>
-
-
-                <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{justifyContent: "center", padding: windowWidth * .05, flex: 1}}>
-                    <Text style={{fontSize: windowWidth * 0.07, fontWeight: "bold", marginBottom: windowHeight * 0.01}}t>{post.attendee.length} people going</Text>
-
-                </KeyboardAvoidingView>
-            </ScrollView>
-
-            <View style={{ flexDirection: "row"}}>
-                <TouchableOpacity onPress={() => console.log("share")} style={styles.fancyButtonContainer}>
-                    <Text style={styles.fancyButtonText}>Share</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={onGoing}  style={styles.fancyButtonContainer}>
-                    <Text style={styles.fancyButtonText}>{goingBtnText}</Text>
-                </TouchableOpacity>
-            </View>
-
-
-        </SafeAreaView>
-    );
-}
-
-
 
 export default EventDetails;
